e326c4652e358d58b9a743ecade2de922f6b36db
Heathcliff <dennis.nathan@gmail.com>
2018-09-26 12:19:30 -0600

    replace-with-alphabet-position problem

---
+++ challenges/replace-with-alphabet-position.js
@@ -0,0 +1,19 @@
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// a being 1, b being 2, etc.
+
+// As an example:
+
+// alphabet_position("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" as a string.
+
+function alphabetPosition(text) {
+    let textArray = text.replace(/[^a-zA-Z]/g, '').split('');
+    textArray.forEach((letter, i) => {
+        let letterAsciiNum = (letter.toLowerCase().charCodeAt(0) - 96).toString();
+        textArray[i] = letterAsciiNum;
+    })
+    return textArray.join(' ');
+}
